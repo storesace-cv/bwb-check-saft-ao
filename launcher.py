7c59cd48b0ef9900a0890b45b7502343023b008f
storesace-cv <153992141+storesace-cv@users.noreply.github.com>
2025-10-07 14:21:06 +0100

    Import sys in launcher

--- launcher.py
+++ launcher.py
@@ -16,6 +16,7 @@ else:  # pragma: no branch - simple happy path
 import argparse
 import importlib
 import subprocess
+import sys
 from importlib import metadata as importlib_metadata
 from pathlib import Path
 from typing import Any, Callable
